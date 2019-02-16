f6657b5349dab3763da730f344708eac374dc6bb
Raphael REME <raphael.reme@orange.fr>
2019-02-16 16:17:23 +0100

    Transform done. Some modification could be done

--- adder.py
+++ adder.py
@@ -21,11 +21,35 @@ s = SatVar('s')
 cout = SatVar('cout')
 
 # Internal variables (if needed)
+s0 = SatVar('s0')
+s1 = SatVar('s1')
+s2 = SatVar('s2')
 
 
-def mk_adder() -> Cnf:
+def mk_or(s, a, b):
+    """Take 2 inputs a, b and 1 output s. Return the cnf of the gate s <=> a | b"""
+    return (a | b | ~s) & (s | ~a) & (s | ~b)
 
-    # TODO: Construct CNF for full adder
-    # (The CNF returned below is pure nonsense...)
-    return (a | ~a) & (b | ~s) & ~cout & (b | cin)
+def mk_and(s, a, b):
+    """Take 2 inputs a, b and one output s. Return the cnf of the gate s <=> a & b"""
+    return (~a | ~b | s) & (~s | a) & (~s | b)
 
+def mk_xor(s, a, b):
+    """Take 2 inputs a, b and 1 output s. Return the cnf of the gate s <=> a ^ b"""
+    return (~s | a | b) & (~s | ~a | ~b) & (s | ~a | b) & (s | a | ~b)
+
+def mk_not(s, a):
+    """Take 1 inputs a and 1 output s. Return the cnf of the gate s <=> ~a"""
+    return (s | a) & (~s | ~a)
+
+def mk_eq(s, a):
+    """Take 1 inputs a and 1 output s. Return the cnf of the gate s <=> a"""
+    return (s | ~a) & (~s | a)
+
+def mk_adder():
+    return mk_xor(s0, a, b) & mk_xor(s, cin, s0) & mk_and(s1, a, b) & mk_and(s2, cin, s0) & mk_or(cout, s1, s2)
+
+def solve():
+    """Return a solution to the adder cnf.
+    A solution is just a hint that the output is well defined (that there is no contradiction in the circuit)"""
+    return Solver().solve(mk_adder())
--- transform.py
+++ transform.py
@@ -5,6 +5,7 @@ import sys
 import circuit.circuit as circ
 from circuit.cnf import SatVar, Solver, Cnf
 from circuit.circuit import Circuit
+from adder import *
 
 # Implementation hints:
 #
@@ -28,13 +29,71 @@ from circuit.circuit import Circuit
 #    code passes all the tests, there is a good chance that it is
 #    correct.
 
-def transform(c: Circuit, prefix: str='') -> Cnf:
+
+
+def f(node, prefix = ''):
+    cnf = Cnf()
+
+    children = node.getChildren()
+    if type(node).__name__ == "Variable":
+        s = SatVar(prefix+node.getName())
+        return s, cnf
+
+    s = SatVar(prefix+ "s" + str(node.getID()))
+    if type(node).__name__ == "Literal":
+        if (node.getValue()):
+            cnf = cnf & s
+        else:
+            cnf = cnf & ~s
+        return s, cnf
+
+    if type(node).__name__ == "BinOp":
+        a, cnf1 = f(node.getChild(0), prefix)
+        b, cnf2 = f(node.getChild(1), prefix)
+        cnf = cnf & cnf1 & cnf2
+        if (node.getOp() == "&"):
+            cnf = cnf & mk_and(s, a, b)
+        elif (node.getOp() == "^"):
+            cnf = cnf & mk_xor(s, a, b)
+        elif (node.getOp() == "|"):
+            cnf = cnf & mk_or(s, a, b)
+        else:
+            raise ValueError("Unrecognized operator " + node.getOp())
+        return s, cnf
+
+    if type(node).__name__ == "UnOp":
+        a, cnf1 = f(node.getChild(0), prefix)
+        cnf = cnf & cnf1
+        if (node.getOp() == "~"):
+            cnf = cnf & mk_not(s, a)
+        else:
+            raise ValueError("Unrecognized operator " + node.getOp())
+        return s, cnf
+
+def transform(c: Circuit, prefix: str=''):
     '''The function transform takes a Circuit c and returns a Cnf obtained by the
     Tseitin transformation of c. The optional prefix string will be used for
     all variable names in the Cnf.
-
     '''
 
-    # TODO: implement me
-    pass
+    cnf = Cnf()
+    for keys in c.getSignals():
+        s = SatVar(prefix+keys)
+        node = c.getEquation(keys)
 
+        children = node.getChildren()
+        if not children:
+            if type(node).__name__ == "Variable":
+                a = SatVar(prefix+node.getName())
+                cnf = cnf & mk_eq(s, a)
+            if type(node).__name__ == "Literal":
+                if (node.getValue()):
+                    cnf = cnf & s
+                else:
+                    cnf = cnf & ~s
+        else:
+            a, cnf1 = f(node, prefix)
+            cnf = cnf & cnf1 & mk_eq(s, a)
+
+
+    return cnf
